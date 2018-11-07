9dcb4e1f1be1be13b974758f1f1a5b143f8bcbd0
Minipasglop <dertest58@yahoo.fr>
2018-11-07 20:08:04 +0100

    Fix: Issue on !ban command

--- src/main/java/discord/bot/command/server/managing/BanCommand.java
+++ src/main/java/discord/bot/command/server/managing/BanCommand.java
@@ -11,7 +11,7 @@ import org.apache.log4j.Logger;
 import java.util.List;
 
 public class BanCommand extends ICommand {
-    private final String HELP = "Ban one / many users from the server if you're allowed to. \nUsage : `!"+ this.commandName +" @UserA @UserB @UserC ... duration Reason `";
+    private final String HELP = "Ban one / many users from the server if you're allowed to. \nUsage : `!"+ this.commandName +" @UserA @UserB @UserC ... Reason `";
     private final String BAN_MESSAGE = "You've been banned because of : ";
     private final String NOT_ALLOWED = "You're not allowed to ban other users... Sadly :)";
     private final String ACTION_PERFORMED = "Bannir : ";
@@ -23,7 +23,7 @@ public class BanCommand extends ICommand {
 
     @Override
     public boolean called(String[] args, MessageReceivedEvent event) {
-        if(args.length != 0 && args[0].equals("help") || args.length < 3) return false;
+        if(args.length != 0 && args[0].equals("help") || args.length < 2) return false;
         else return true;
     }
 
@@ -32,7 +32,7 @@ public class BanCommand extends ICommand {
         if(event.getMember().getPermissions().contains(Permission.BAN_MEMBERS)) {
             List<Member> targetedUsers = event.getMessage().getMentionedMembers();
             for (Member curr : targetedUsers) {
-                event.getGuild().getController().ban(curr, Integer.parseInt(args[args.length - 2]), BAN_MESSAGE + args[args.length - 1]).queue();
+                event.getGuild().getController().ban(curr,1, BAN_MESSAGE + args[args.length - 1]).queue();
                 logger.log(Level.INFO, ACTION_PERFORMED + curr.getUser().getName() + " par " + event.getAuthor().getName() + " sur le serveur : " + event.getGuild().getName());
             }
         }else{
