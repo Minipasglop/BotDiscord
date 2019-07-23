eb72471315d26e4ef533ba6b46e8325b02c31749
Minipasglop <dertest58@yahoo.fdr>
2019-07-23 23:15:46 +0200

    Fix(Issue with music player): Upgraded dependency lavaplayer

--- src/main/java/discord/bot/utils/audio/AudioPlayerSendHandler.java
+++ src/main/java/discord/bot/utils/audio/AudioPlayerSendHandler.java
@@ -12,32 +12,19 @@ public class AudioPlayerSendHandler implements AudioSendHandler {
     private final AudioPlayer audioPlayer;
     private AudioFrame lastFrame;
 
-    /**
-     * @param audioPlayer Audio player to wrap.
-     */
     public AudioPlayerSendHandler(AudioPlayer audioPlayer) {
         this.audioPlayer = audioPlayer;
     }
 
     @Override
     public boolean canProvide() {
-        if (lastFrame == null) {
-            lastFrame = audioPlayer.provide();
-        }
-
+        lastFrame = audioPlayer.provide();
         return lastFrame != null;
     }
 
     @Override
     public byte[] provide20MsAudio() {
-        if (lastFrame == null) {
-            lastFrame = audioPlayer.provide();
-        }
-
-        byte[] data = lastFrame != null ? lastFrame.data : null;
-        lastFrame = null;
-
-        return data;
+        return lastFrame.getData();
     }
 
     @Override
